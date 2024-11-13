9eaf84eafc56ffb4795b96efadcdc4124ef9ec01
Nurdin <mrsunnysummer@gmail.com>
2024-11-14 05:39:13 +0700

    change: update email, address and phone to be optional

--- src/responden/responden.validation.ts
+++ src/responden/responden.validation.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 export class RespondenValidation {
     static readonly SAVE = z.object({
         name: z.string().min(1).max(255),
-        email: z.string().email().min(1).max(255),
-        address: z.string().min(1).max(255),
-        phone: z.string().min(1).max(20),
+        email: z.string().email().min(1).max(255).optional(),
+        address: z.string().min(1).max(255).optional(),
+        phone: z.string().min(1).max(20).optional(),
         age: z.number().min(1),
         education: z.string().min(1).max(255),
         profession: z.string().min(1).max(255),
